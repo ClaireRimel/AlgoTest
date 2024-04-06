9f204246006dd115afc6a8ce024f59213718960f
Claire <43319552+ClaireRimel@users.noreply.github.com>
2024-04-06 21:26:06 +0200

    New: Create a file for the 30-day challenge
    + improve the summary

--- Algotest.playground/Pages/Summary.xcplaygroundpage/Contents.swift
+++ Algotest.playground/Pages/Summary.xcplaygroundpage/Contents.swift
@@ -2,8 +2,9 @@
 /*:
  # Summary
  ## Algorithm Paractices
- ### [Hackerrank](https://www.hackerrank.com/challenges)
+ ### [Hackerrank](https://www.hackerrank.com/dashboard)
  1. [Warmup](Warmup)
+ 2. [30 Days Of Code](30%20Days%20Of%20Code)
 
  ### [LeetCode](https://leetcode.com/problems)
  1. [Problems List](Problems%20List)
