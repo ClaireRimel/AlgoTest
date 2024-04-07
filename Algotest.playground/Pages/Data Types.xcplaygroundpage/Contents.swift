0895706b8a494b046bafeb574a1856584e26dd36
Claire <43319552+ClaireRimel@users.noreply.github.com>
2024-04-07 16:14:09 +0200

    New: add resolution of Data Types Problem

--- Algotest.playground/Pages/Data Types.xcplaygroundpage/Contents.swift
+++ Algotest.playground/Pages/Data Types.xcplaygroundpage/Contents.swift
@@ -1,7 +1,20 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+let i = 4
+let d = 4.0
+let s = "HackerRank "
 
-//: [Next](@next)
+// Declare variables of the required types
+var myInt: Int
+var myDouble: Double
+var myString: String
+
+// Save inputs
+myInt = 4
+myDouble = 14.0
+myString = "Hello"
+
+// Perform and print operations
+print(i + myInt)
+print(d + myDouble)
+print(s + myString)
