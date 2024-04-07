507454d406957837db3c410885de6e7a6d1ab7d5
Claire <43319552+ClaireRimel@users.noreply.github.com>
2024-04-07 17:03:26 +0200

    New: warmup - A Very Big Sum

--- Algotest.playground/Pages/Warmup.xcplaygroundpage/Contents.swift
+++ Algotest.playground/Pages/Warmup.xcplaygroundpage/Contents.swift
@@ -2,4 +2,5 @@
  # [Warmup](https://www.hackerrank.com/domains/algorithms?filters%5Bstatus%5D%5B%5D=unsolved&badge_type=problem-solving)
  1. [Simple Array Sum](Simple%20Array%20Sum)
  2. [Compare The Triplets](Compare%20The%20Triplets)
+ 3. [A Very Big Sum](A%20Very%20Big%20Sum)
  */
