31c45cfd6a7e86b00a703020a7b291ba0690d004
Claire <43319552+ClaireRimel@users.noreply.github.com>
2024-04-07 16:53:12 +0200

    New: Resolve the problem for Day 2

--- Algotest.playground/Pages/Operators.xcplaygroundpage/Contents.swift
+++ Algotest.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -1,1 +1,18 @@
+/*:
+# Task
+ 
+ Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+ */
+func solve(meal_cost: Double, tip_percent: Int, tax_percent: Int) {
+    
+    let tipsAmount = meal_cost * Double(tip_percent)/100
+    let taxAmount = meal_cost * Double(tax_percent)/100
+    var totalCost = meal_cost + tipsAmount + taxAmount
+    totalCost.round()
+    
+    print(Int(totalCost))
+}
 
+solve(meal_cost: 10.25,
+      tip_percent: 17,
+      tax_percent: 8)
