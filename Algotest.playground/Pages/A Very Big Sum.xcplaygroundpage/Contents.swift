142a0533f101560f0dea0f8d4d4ded1aecaa2085
Claire <43319552+ClaireRimel@users.noreply.github.com>
2024-04-07 17:24:16 +0200

    Chg: Improve A Very Big Sum Solution

--- Algotest.playground/Pages/A Very Big Sum.xcplaygroundpage/Contents.swift
+++ Algotest.playground/Pages/A Very Big Sum.xcplaygroundpage/Contents.swift
@@ -2,9 +2,7 @@
 */
 
 func aVeryBigSum(ar: [Int]) -> Int {
-    return ar.reduce(0) { partialResult, next in
-        partialResult + next
-    }
+    return ar.reduce(0, +)
 }
 
 aVeryBigSum(ar: [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]) // 5000000015
