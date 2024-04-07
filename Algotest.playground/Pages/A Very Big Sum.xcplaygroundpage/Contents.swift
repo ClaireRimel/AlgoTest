01ee6b903595bfe37e9d0947f9495521295e6386
Claire <43319552+ClaireRimel@users.noreply.github.com>
2024-04-07 17:18:39 +0200

    New: Resolve A Very Big Sum Problem

--- Algotest.playground/Pages/A Very Big Sum.xcplaygroundpage/Contents.swift
+++ Algotest.playground/Pages/A Very Big Sum.xcplaygroundpage/Contents.swift
@@ -1,7 +1,10 @@
-//: [Previous](@previous)
+/*: I apply the same approach as in the Simple Array Sum Problem because utilising the `.reduce()` function results in 𝘖(𝓃)) complexity. This means that the time required to execute the algorithm grows linearly with the size of the input, making such algorithms suitable for handling large inputs effectively up to a certain point.
+*/
 
-import Foundation
+func aVeryBigSum(ar: [Int]) -> Int {
+    return ar.reduce(0) { partialResult, next in
+        partialResult + next
+    }
+}
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+aVeryBigSum(ar: [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]) // 5000000015
